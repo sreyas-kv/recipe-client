430730ab2c981df259057b98ba429b823bbe6f3e
𝓢𝓇𝓮𝓎𝒶𝓈 👽👾🤖 <sreyas.selenium@gmail.com>
2020-02-23 20:40:30 -0600

    changed server route

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    API_URL: process.env.REACT_APP_API_URL || 'http://recipebook-server.herokuapp.com/v1',
+    API_URL: process.env.REACT_APP_API_URL || 'https://recipebook-server.herokuapp.com/v1',
 }
